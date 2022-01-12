db19e6da24bb088138467c4892f71b34ab60adeb
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2022-01-12 21:35:39 +0300

    Drop CommonCompilerArguments.optInDeprecated (KT-22956)

--- compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/CommonCompilerArguments.kt
+++ compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/CommonCompilerArguments.kt
@@ -75,8 +75,7 @@ abstract class CommonCompilerArguments : CommonToolArguments() {
 
     @Argument(
         value = "-opt-in",
-        // Uncomment after deletion of optInDeprecated
-        // deprecatedName = "-Xopt-in",
+        deprecatedName = "-Xopt-in",
         valueDescription = "<fq.name>",
         description = "Enable usages of API that requires opt-in with an opt-in requirement marker with the given fully qualified name"
     )
@@ -165,14 +164,6 @@ abstract class CommonCompilerArguments : CommonToolArguments() {
         description = "Enable, but don't propagate usages of experimental API for marker annotation with the given fully qualified name"
     )
     var useExperimental: Array<String>? by FreezableVar(null)
-
-    // NB: we have to keep this flag for some time due to bootstrapping problems
-    @Argument(
-        value = "-Xopt-in",
-        valueDescription = "<fq.name>",
-        description = "Enable usages of API that requires opt-in with an opt-in requirement marker with the given fully qualified name"
-    )
-    var optInDeprecated: Array<String>? by FreezableVar(null)
 
     @Argument(
         value = "-Xproper-ieee754-comparisons",
@@ -418,13 +409,7 @@ abstract class CommonCompilerArguments : CommonToolArguments() {
                     WARNING, "'-Xuse-experimental' is deprecated and will be removed in a future release, please use -opt-in instead"
                 )
             }
-            val optInDeprecatedFqNames = optInDeprecated?.toList().orEmpty()
-            if (optInDeprecatedFqNames.isNotEmpty()) {
-                collector.report(
-                    WARNING, "'-Xopt-in' is deprecated and will be removed in a future release, please use -opt-in instead"
-                )
-            }
-            put(AnalysisFlags.optIn, useExperimentalFqNames + optInDeprecatedFqNames + optIn?.toList().orEmpty())
+            put(AnalysisFlags.optIn, useExperimentalFqNames + optIn?.toList().orEmpty())
             put(AnalysisFlags.expectActualLinker, expectActualLinker)
             put(AnalysisFlags.explicitApiVersion, apiVersion != null)
             put(AnalysisFlags.allowResultReturnType, allowResultReturnType)
