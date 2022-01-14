d180c2014dd0f2888dec6c113de3924bad2f53c2
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-01-14 19:29:39 +0300

    Fix false negative UPPER_BOUND_VIOLATED with typealiases in supertypes
    ^KT-50797 Fixed

---
+++ compiler/testData/diagnostics/tests/typealias/abbreviatedSupertypes.kt
@@ -0,0 +1,22 @@
+// SKIP_TXT
+// !LANGUAGE: -ReportMissingUpperBoundsViolatedErrorOnAbbeviationAtSupertypes
+
+interface I
+open class TK<T : I, K : I>
+
+typealias One<X> = TK<X, X>
+typealias Both<T, K> = TK<T, K>
+
+object O1 : <!UPPER_BOUND_VIOLATED_IN_TYPEALIAS_EXPANSION_WARNING!>One<Any><!>() // compiler error expected
+object O2 : <!UPPER_BOUND_VIOLATED_IN_TYPEALIAS_EXPANSION!>Both<Any, Any><!>()
+
+class A1<T : <!UPPER_BOUND_VIOLATED_IN_TYPEALIAS_EXPANSION_WARNING!>One<Any><!>>
+class A2<T : One<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><Any, Any><!>>
+
+fun foo1(x: One<<!UPPER_BOUND_VIOLATED!>Any<!>>) {}
+fun foo2(x: Both<<!UPPER_BOUND_VIOLATED!>Any<!>, <!UPPER_BOUND_VIOLATED!>Any<!>>) {}
+
+fun main() {
+    One<<!UPPER_BOUND_VIOLATED!>Any<!>>()
+    Both<<!UPPER_BOUND_VIOLATED!>Any<!>, <!UPPER_BOUND_VIOLATED!>Any<!>>()
+}
