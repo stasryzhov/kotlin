05f6a58c75679182f8a20bd4b2edb0c63161e950
Anton Lakotka <anton.lakotka@jetbrains.com>
2022-01-17 12:32:54 +0100

    [MPP, KPM]: Use new configureSingleVariantPublication API

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/pm20/LegacyMappedVariant.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/pm20/LegacyMappedVariant.kt
@@ -187,7 +187,7 @@ internal fun mapTargetCompilationsToKpmVariants(target: AbstractKotlinTarget, pu
                 is JointAndroidKotlinTargetComponent -> kotlinComponent.usages
                 else -> error("unexpected type of kotlinComponent in legacy variant mapping: ${kotlinComponent.javaClass}")
             }
-            val configurationNames = usages.map { target.project.configurations.getByName(it.dependencyConfigurationName) }
+            val configurations = usages.map { target.project.configurations.getByName(it.dependencyConfigurationName) }
 
             // Include Sources
             moduleHolder.whenPublicationAssigned { publication ->
@@ -202,7 +202,7 @@ internal fun mapTargetCompilationsToKpmVariants(target: AbstractKotlinTarget, pu
             VariantPublishingConfigurator.get(target.project).configureSingleVariantPublishing(
                 variant,
                 moduleHolder,
-                configurationNames
+                configurations
             )
         }
     }
