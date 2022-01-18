53ba01aa1f40aec210d432e60f4a20000581afcb
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-01-18 16:00:35 +0100

    [WASM] Any.toString implementation

--- libraries/stdlib/wasm/builtins/kotlin/Any.kt
+++ libraries/stdlib/wasm/builtins/kotlin/Any.kt
@@ -49,6 +49,10 @@ public open class Any {
     /**
      * Returns a string representation of the object.
      */
-    // TODO: Implement
-    public open fun toString(): String = "[Object object]"
+    public open fun toString(): String {
+        val typeData = getTypeInfoTypeDataByPtr(typeInfo)
+        val qualifiedName: String =
+            if (typeData.packageName.isEmpty()) typeData.typeName else "${typeData.packageName}.${typeData.typeName}"
+        return "$qualifiedName@${hashCode()}"
+    }
 }
